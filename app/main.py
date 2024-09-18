c354800d210f9f16fbd063082350bbc9016d5e00
Hugo Ludemann <hugo.ludemann@gmail.com>
2024-09-18 12:52:41 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,14 +7,31 @@ from dataclasses import dataclass
 database_file_path = sys.argv[1]
 command = sys.argv[2]
 
-if command == ".dbinfo":
-    with open(database_file_path, "rb") as database_file:
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
 
-        # Uncomment this to pass the first stage
-        database_file.seek(16)  # Skip the first 16 bytes of the header
-        page_size = int.from_bytes(database_file.read(2), byteorder="big")
-        print(f"database page size: {page_size}")
+def page_size(database_file_path):
+    with open(database_file_path, "rb") as database_file:
+        database_file.seek(16)
+        return int.from_bytes(database_file.read(2), byteorder="big")
+
+
+def page_type(database_file_path):
+    with open(database_file_path, "rb") as database_file:
+        database_file.seek(100)
+        return hex(int.from_bytes(database_file.read(1), byteorder="big"))
+
+
+def n_tables(database_file_path):
+    with open(database_file_path, "rb") as database_file:
+        # Skip the database header and navigate B-tree page header.
+        database_file.seek(100 + 3)
+        return int.from_bytes(database_file.read(2), byteorder="big")
+
+
+if command == ".dbinfo":
+    print(f"database page size: {page_size(database_file_path)}")
+    print(f"number of tables: {n_tables(database_file_path)}")
+elif command == "dev":
+    print(f"dev: {page_type(database_file_path)}")
 else:
     print(f"Invalid command: {command}")
+
